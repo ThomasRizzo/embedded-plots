e6257b531b35a4cc8a608dd1535ea5a26adeee2a
Kacper Leśniański <kacper.lesnianski@wp.pl>
2022-08-13 19:00:41 +0200

    Single plot takes slices of curves + colors

--- examples/multiple_curves.rs
+++ examples/multiple_curves.rs
@@ -10,6 +10,8 @@ use embedded_plots::{
 
 fn main() -> Result<(), core::convert::Infallible> {
     let mut display: SimulatorDisplay<Rgb565> = SimulatorDisplay::new(Size::new(480, 272));
+
+    //min(x), max(x), min(y) and max(y) must be equal for each curve in order to display the plot correctly
     let data = vec![
         PlotPoint { x: 0, y: 0 },
         PlotPoint { x: 1, y: 2 },
@@ -17,8 +19,17 @@ fn main() -> Result<(), core::convert::Infallible> {
         PlotPoint { x: 3, y: 0 },
     ];
 
+    let data2 = vec![
+        PlotPoint { x: 0, y: 0 },
+        PlotPoint { x: 1, y: 2 },
+        PlotPoint { x: 2, y: 1 },
+        PlotPoint { x: 3, y: 1 },
+    ];
+
     let curve = Curve::from_data(data.as_slice());
-    let curve_list = [(curve, RgbColor::YELLOW)];
+    let curve2 = Curve::from_data(data2.as_slice());
+    let curve_list = [(curve, RgbColor::YELLOW), (curve2, RgbColor::BLUE)];
+
     let plot = SinglePlot::new(&curve_list, Scale::RangeFraction(3), Scale::RangeFraction(2))
         .into_drawable(Point { x: 50, y: 10 }, Point { x: 430, y: 250 })
         .set_color(RgbColor::YELLOW)
